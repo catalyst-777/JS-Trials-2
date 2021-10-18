171ccc37c6ed341f7425678d0f545fd616f80609
Courtney Doss <777.catalyst@gmail.com>
2021-10-18 16:51:18 -0500

    Completed Further Study

--- furtherStudy.js
+++ furtherStudy.js
@@ -1,9 +1,52 @@
 'use strict';
 
 function wordsInCommon(words1, words2) {
-  // Replace this with your code
+  const setWords1 = new Set(words1);
+  const setWords2 = new Set(words2);
+
+  const results = new Set();
+
+  for (let word of setWords1) {
+    if (setWords2.has(word)) {
+      results.add(word)
+    }
+  }
+
+  return Array.from(results);
 }
 
+let kidLanguage = ["bagon", "baltoy", "yamask", "starly", "nosepass", "kalob", "nicky", "booger"]
+
 function kidsGame(names) {
-  // Replace this with your code
+  let output = [names.shift()];
+
+  const firstLetterWords = {};
+
+  for(let name of names) {
+    if(!firstLetterWords[name[0]]){
+      firstLetterWords[name[0]] = [name];
+    } 
+    else {
+      firstLetterWords[name[0]].push(name);
+    }
+  }
+
+  console.log('first word: ' + output);
+  
+  while (true) {
+    let lenOutput = output.length -1;
+    let lastWord = output[lenOutput];
+    console.log('last word: ' + lastWord);
+    
+    if (lastWord === undefined) {
+      console.log('inside conditional')
+      break;
+    }
+
+    let lastLetter = lastWord[(lastWord.length) - 1];
+
+    let word = firstLetterWords[lastLetter].shift();
+    output.push(word);
+  }
+  return output;
 }
